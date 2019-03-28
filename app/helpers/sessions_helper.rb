0a0f59df36094944cbce6fb231e3ac1a57d187fe
kumasss <soccer.kumakuma@gmail.com>
2019-03-28 15:02:23 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,20 @@
 module SessionsHelper
-  # 渡されたユーザーでログインする
+
+  # 渡されたユーザーをログイン
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  # ユーザーを永続的に復元できるようになった
+  # 永続セッションとしてユーザーを記憶する
   def remember(user)
-    user.remember # => DB: remember_digest
+    user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
   end
 
   def forget(user)
@@ -17,6 +23,7 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  # 記憶トークン (cookie) に対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -42,5 +49,15 @@ module SessionsHelper
     @current_user = nil
   end
 
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
+
